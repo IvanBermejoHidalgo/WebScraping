2b62795553ae22228cc8d7712901f5dae92cb40a
IvanBermejoHidalgo <ibermejo@elpuig.xeill.net>
2025-03-04 15:48:25 +0100

    Cambiado links a bootstrap

--- public/views/admin/drivers/add_driver.php
+++ public/views/admin/drivers/add_driver.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Añadir Piloto</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* Estilos personalizados */
         body {
--- public/views/home.php
+++ public/views/home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home - Fórmula 1</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Estilos personalizados -->
     <style>
         body {
